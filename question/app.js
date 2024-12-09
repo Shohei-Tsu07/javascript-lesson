596308274055433ab5fbba2ba0e9f37b26cf1a44
unknown <s.tsuchida07@gmail.com>
2024-12-09 18:51:34 +0900

    Q4

--- question/app.js
+++ question/app.js
@@ -27,3 +27,25 @@ let user = {
 };
 
 console.log(user.age);
+
+// Q4
+let playerList = [
+  {
+    name: 'John',
+    age: 26,
+    favorites: ['Card Game', 'Basket Ball', 'Programming'],
+  },
+  {
+    name: 'Bob',
+    age: 33,
+    favorites: ['Tinder', 'The Legend of Zelda'],
+  },
+  {
+    name: 'Michael',
+    age: 22,
+    favorites: ['Football', 'Smash Bros.'],
+  },
+];
+
+console.log( playerList[1].favorites[1]);
+
