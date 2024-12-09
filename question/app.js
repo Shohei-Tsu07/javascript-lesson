3c18c6632e896e75927232f1818bf15ec49ecfed
unknown <s.tsuchida07@gmail.com>
2024-12-09 18:23:23 +0900

    Q2

--- question/app.js
+++ question/app.js
@@ -4,3 +4,16 @@ let age = 28;
 let greet = "私のニックネームは" + nickname + "です。" + "年齢は" + age + "歳です。";
 
 console.log(greet);
+
+// Q2
+let languages = [
+  "JavaScript", 
+  "PHP",
+  "Ruby",
+  "Python",
+  "Go",
+]
+
+let languagesText = `私の好きな言語は${languages[0]}です。次は${languages[3]}も勉強したいです。`;
+
+console.log(languagesText);
